8bc05e7b01add6f7bcf5637f768498e47d2754ed
Michael J Baumli <michaeljbaumli@gmail.com>
2021-05-17 20:22:19 -0500

    Imported the stats module for the central-with-statistics.py program This was much easier.

--- central-with-statistics.py
+++ central-with-statistics.py
@@ -12,8 +12,19 @@ statistics.mode
 
 statistics.median
 
+#Variable
+grades = [85,93,45,89,85]
 
 
 
+classgrade = statistics.mean(grades)
 
+print("The mean grade for the class is:", classgrade)
 
+mediangrade = statistics.mode(grades)
+
+print("The median grade is: ", mediangrade)
+
+modegrade = statistics.mode(grades)
+
+print("The mode grade is: ",modegrade)
